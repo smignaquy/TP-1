11844c169a362f7650e0284842bc55caad670aa9
smignaquy <smignaquy@ydesa.edu.ar>
2022-09-05 14:45:50 -0300

    template

---
+++ template.py
@@ -0,0 +1,29 @@
+############################# NO TOCAR ESTE CÓDIGO ############################
+from random import randint
+
+def sacar_carta():
+    '''
+    Esta función toma una carta de un mazo de forma aleatoria. La carta está numerada del 1 al 10 (inclusive).
+
+    params:
+        Esta función no tiene parámetros de entrada.
+    out:
+        carta: int. El número de la carta sacada.
+    '''
+    carta = randint(1,10)
+    return carta
+
+
+######################## EJEMPLO DE USO DE SACAR_CARTA ########################
+#c = sacar_carta()
+#print(c)
+#En la consola se vería:    8
+
+########################### AQUÍ COMIENZA TU CÓDGIO ###########################
+
+while(): # Repetición del juego
+    #Tu código va acá
+    while(): # Turno del jugador
+        #Tu código va acá
+    while(): # Turno del crupier
+    	#Tu código va acá
